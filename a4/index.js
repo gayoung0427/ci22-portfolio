4172753fb3c8370ed75e55529c0f3015413a3eba
gayoung0427 <dog777@newschool.edu>
2022-03-10 22:46:47 -0500

    a4

---
+++ a4/index.js
@@ -0,0 +1,49 @@
+var image= document.getElementById("imageOne");
+function change1(){
+    if (image.getAttribute('src') == "image/after1.png"){
+        image.src = "image/before1.png";
+    }
+    else {
+        image.src = "image/after1.png";
+    }
+}
+
+var image2= document.getElementById("imagetwo");
+function change2(){
+    if (image2.getAttribute('src') == "image/before2.png"){
+        image2.src = "image/after2.png";
+    }
+    else {
+        image2.src = "image/before2.png";
+    }
+}
+
+var image3= document.getElementById("imagethree");
+function change3(){
+    if (image3.getAttribute('src') == "image/before3.png"){
+        image3.src = "image/after3.png";
+    }
+    else {
+        image3.src = "image/before3.png";
+    }
+}
+
+var image4= document.getElementById("imagefour");
+function change4(){
+    if (image4.getAttribute('src') == "image/before4.png"){
+        image4.src = "image/after4.png";
+    }
+    else {
+        image4.src = "image/before4.png";
+    }
+}
+
+var image5= document.getElementById("imagefive");
+function change5(){
+    if (image5.getAttribute('src') == "image/before5.png"){
+        image5.src = "image/after5.png";
+    }
+    else {
+        image5.src = "image/before5.png";
+    }
+}
