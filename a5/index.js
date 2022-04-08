f148026ac8b82538216c038b7de10fd82845264c
gayoung0427 <dog777@newschool.edu>
2022-04-07 21:05:41 -0400

    a5

--- a5/index.js
+++ a5/index.js
@@ -1,3 +1,7 @@
 function clicked(element) {
     document.getElementById(element.id).style.opacity = "0";
   }
+
+function myFunction() {
+    document.getElementById("background").style.backgroundImage = "url(image/darkbackground.jpg)";
+ }
